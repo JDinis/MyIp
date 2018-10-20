2a5b38afd3c38e0766fa21fedc81947a1e1f0336
JDinis <j.p.dinis89@gmail.com>
2018-10-20 18:05:04 +0100

    Interface Update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let http = require('http');
 var geoip = require('geoip-lite');
 var publicIp = require('public-ip');
+var cities = require('cities');
 
 http.createServer(function(req,res){
 	res.setHeader('Content-Type', 'text/html');
@@ -8,9 +9,16 @@ http.createServer(function(req,res){
 		res.write(ip);
 		var rsp = geoip.lookup(ip);
 		res.write("</br>");
-		res.end(rsp.country);
+		res.write("City: "+cities.gps_lookup(rsp.ll[0],rsp.ll[1]).toString());
+		res.write("</br>");
+		res.write("Country: "+rsp.country);
+		res.write("</br>");
+		res.write("Region: "+rsp.region);
 		console.log(rsp);
-		console.log(ip);
+		console.log(rsp.ll);
+		console.log(rsp.ll[0]);
+		console.log(rsp.ll[1]);
+		console.log(cities.gps_lookup(""+rsp.ll[0],""+rsp.ll[1]));
 	});
 }).listen(8080);
 
